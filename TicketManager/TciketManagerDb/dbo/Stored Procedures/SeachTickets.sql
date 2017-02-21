875e29f961b24e107cc78db4d0dce7a7b908aea6
Mehendra Munasinghe <mehendra@gmail.com>
2017-02-21 20:48:05 +1300

    database stuff

---
+++ TicketManager/TciketManagerDb/dbo/Stored Procedures/SeachTickets.sql
@@ -0,0 +1,51 @@
+﻿create procedure dbo.SeachTickets
+	@TicketNumber varchar(10) = null,
+	@AgentCode varchar(10) = null,
+	@TicketStatusCode varchar(4) = null,
+	@Category varchar(50) = null,
+	@RcordCount int = 0,
+	@RecordsPerPage int = 0,
+	@PagingStartIndex int = 1,
+	@TotalRecords int out
+as begin
+
+--test data
+--set @AgentCode = 'CHA'
+--set @TicketNumber = '8'
+--set @RecordsPerPage = 40
+
+declare @AllRecords as table
+(
+	[idx] int not null identity (1,1),
+	[TicketNumber] [varchar](10) NOT NULL,
+	[AgentCode] [varchar](10) NULL,
+	[Category] [varchar](50) NULL,
+	[TicketStatusCode] [varchar](4) NULL,
+	[ArrivedAt] [datetime] NULL,
+	[ArrivalConfirmedBy] [varchar](255) NULL,
+	[AgentName] [varchar](255) NULL,
+	[TicketStatus] nvarchar(50) null
+)
+insert into @AllRecords( [TicketNumber]
+      ,[AgentCode]
+      ,[Category]
+      ,[TicketStatusCode]
+      ,[ArrivedAt]
+      ,[ArrivalConfirmedBy]
+	  ,AgentName
+	  ,[TicketStatus])
+select t.*,a.AgentName,ts.TicketStatus  from dbo.TicketsIssued t inner join dbo.Agent a
+on t.AgentCode = a.AgentCode left join [dbo].[TicketStatus] ts
+on ts.TicketStatusCode = t.TicketStatusCode
+where t.AgentCode = case when @AgentCode is null then t.AgentCode else @AgentCode  end
+and t.TicketNumber like case when @TicketNumber is null then t.TicketNumber else @TicketNumber + '%' end
+and isnull(t.TicketStatusCode,'') = case when @TicketStatusCode is null then isnull(t.TicketStatusCode,'') else @TicketStatusCode end
+
+
+SELECT @RcordCount = count(*) from @allRecords
+
+select @TotalRecords = @RcordCount
+
+select * from @AllRecords
+WHERE idx between (@RecordsPerPage * (@PagingStartIndex -1)) +1 and @RecordsPerPage * @PagingStartIndex
+end
---
+++ TicketManager/TciketManagerDb/dbo/Tables/TicketsIssued.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TicketsIssued] (
+    [TicketNumber]       VARCHAR (17)  NOT NULL,
+    [AgentCode]          VARCHAR (4)   NOT NULL,
+    [Category]           VARCHAR (50)  NULL,
+    [TicketStatusCode]   VARCHAR (4)   NULL,
+    [ArrivedAt]          DATETIME      NULL,
+    [ArrivalConfirmedBy] VARCHAR (255) NULL,
+    [Paid]               DATE          NULL,
+    [Notes]              VARCHAR (500) NULL,
+    CONSTRAINT [PK_TicketsIssued] PRIMARY KEY CLUSTERED ([TicketNumber] ASC),
+    CONSTRAINT [fk_Agent] FOREIGN KEY ([AgentCode]) REFERENCES [dbo].[Agent] ([AgentCode]),
+    CONSTRAINT [fk_TicketStatus] FOREIGN KEY ([TicketStatusCode]) REFERENCES [dbo].[TicketStatus] ([TicketStatusCode])
+);
+
