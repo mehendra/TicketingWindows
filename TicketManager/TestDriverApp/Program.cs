a8553c7322872b4699db7ec7c5852dc8e8883add
shasinda <shasinda@icloud.com>
2022-06-22 18:11:47 +1200

    added supporting class and methods to connect to Firebase DB and write to it

---
+++ TicketManager/TestDriverApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Models.APIService;
+using TicketManagerFirebaseDb;
+
+class Program {
+
+
+    public static void Main(String[] args)
+    {
+        Console.Write("Hello World ");
+
+        FireBaseDbService dbservice = new FireBaseDbService();
+        dbservice.Init();
+        ApiTicketsIssued ticketsIssued = new ApiTicketsIssued()
+        {
+            TicketId = 0001,
+            Agent = "SHAN",
+            Category = "VIP",
+            TicketStatusCode = "SPAD",
+            Zone = "VIP",
+            SoldTo = "Shasinda",
+            TicketNumber = "VIP001",
+            Paid = "22/06/2022",
+            Notes = "Great Guy, Free tickets for future okm events"
+
+        };
+
+        dbservice.writeToDb(ticketsIssued);
+    }
+
+}
---
+++ TicketManager/TicketManagerFirebaseDb/TestDriver.cs
@@ -0,0 +1,34 @@
+﻿using Models.APIService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManagerFirebaseDb
+{
+    internal class TestDriver
+    {
+
+        static void Main(String[] args) {
+            FireBaseDbService dbservice = new FireBaseDbService();
+            dbservice.Init();
+            ApiTicketsIssued ticketsIssued = new ApiTicketsIssued()
+            {
+                TicketId = 0001,
+                Agent = "SHAN",
+                Category = "VIP",
+                TicketStatusCode = "SPAD",
+                Zone = "VIP",
+                SoldTo = "Shasinda",
+                TicketNumber = "VIP001",
+                Paid = "22/06/2022",
+                Notes = "Great Guy, Free tickets for future okm events"
+
+            };
+
+            dbservice.writeToDb(ticketsIssued);
+ 
+         }
+    }
+}
