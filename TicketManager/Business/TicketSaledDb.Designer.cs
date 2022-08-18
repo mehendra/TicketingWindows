d0a547644abbd4399e9748c67610a30684509ee5
mehendra <mehendra@gmail.com>
2022-08-18 14:04:56 +1200

    upgraded .net version

--- TicketManager/Business/TicketSaledDb.Designer.cs
+++ TicketManager/Business/TicketSaledDb.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Git\TicketManager\Business\TicketSaledDb.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\mehen\Documents\GitHub\TicketingWindows\TicketManager\Business\TicketSaledDb.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
